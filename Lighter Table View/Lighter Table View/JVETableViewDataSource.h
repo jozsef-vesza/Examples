fb00ec87a833b41a3741a4e1cb38f09bf866fee4
József Vesza <jozsef.vesza@outlook.com>
2014-06-09 08:39:46 +0200

    Comments added

--- Lighter Table View/Lighter Table View/JVETableViewDataSource.h
+++ Lighter Table View/Lighter Table View/JVETableViewDataSource.h
@@ -6,6 +6,9 @@
 //  Copyright (c) 2014 Jozsef Vesza. All rights reserved.
 //
 
+/**
+ *  Protocol for cell configurator classes
+ */
 @protocol JVECellConfiguratorDelegate <NSObject>
 
 - (NSString *)fetchReuseIdentifierForObject:(id)object;
@@ -13,8 +16,20 @@
 
 @end
 
+/**
+ *  Class to encapsulate UITableViewDataSource logic
+ */
 @interface JVETableViewDataSource : NSObject<UITableViewDataSource>
 
+/**
+ *  Custom initializer
+ *
+ *  @param tableView        The table view that will use this instance as datasource
+ *  @param items            The items to show in the table view
+ *  @param cellConfigurator The object responsible for cell configuration
+ *
+ *  @return An instance of JVETableViewDataSource
+ */
 - (instancetype)initWithTableView:(UITableView *)tableView withItems:(NSArray *)items withCellConfigurator:(id<JVECellConfiguratorDelegate>)cellConfigurator;
 
 @end
